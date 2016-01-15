6ed111e0cff170ef56a905b4ea715eefdcc52e15
Daniel <danielbhugon@gmail.com>
2016-01-15 22:13:44 +0000

    Returns word count as well as message

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -16,12 +16,28 @@ describe('fizzbuzz', function() {
 	
 	describe('#message(from, to)', function() {
 		
-		it('should return the expected string for the first 20 numbers', function() {
-			assert.equal('1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz ', fizzbuzz.createMessage(1, 20));
+		it('should return the expected message string for the first 20 numbers', function() {
+			assert.equal('1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz ', fizzbuzz.createMessage(1, 20).message);
+		});
+		
+		it('should return the expected word count for the first 20 numbers', function() {
+			var expectedCount = {
+				fizz : 4,
+				buzz : 3,
+				fizzbuzz : 1,
+				lucky : 2,
+				integer : 10
+			}
+			
+			assert.deepEqual(expectedCount, fizzbuzz.createMessage(1, 20).counts);
 		});
 		
 		it('should return the expected string for the numbers 21 through 25', function() {
-			assert.equal('fizz 22 lucky fizz buzz ', fizzbuzz.createMessage(21, 25));
+			assert.equal('fizz 22 lucky fizz buzz ', fizzbuzz.createMessage(21, 25).message);
 		});
+		
+		
+		
 	});
+	
 })
