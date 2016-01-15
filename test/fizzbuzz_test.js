9df7bc8cbb2883911f9b2b39b83dcd0a198cab5f
Daniel <danielbhugon@gmail.com>
2016-01-15 21:51:50 +0000

    Add 'lucky' behaviour and remove use of multiple return statements

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -5,7 +5,7 @@ describe('fizzbuzz', function() {
 	
 	describe('#translate(number)', function () {
 		
-		var testCases = ['fizzbuzz', '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz', '16'];
+		var testCases = ['fizzbuzz', '1', '2', 'lucky', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', 'lucky', '14', 'fizzbuzz', '16'];
 		
 		testCases.forEach(function(expected, index){
 			it('should return ' + expected + ' for input ' + index, function () {
@@ -17,11 +17,11 @@ describe('fizzbuzz', function() {
 	describe('#message(from, to)', function() {
 		
 		it('should return the expected string for the first 20 numbers', function() {
-			assert.equal('1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz ', fizzbuzz.createMessage(1, 20));
+			assert.equal('1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz ', fizzbuzz.createMessage(1, 20));
 		});
 		
 		it('should return the expected string for the numbers 21 through 25', function() {
-			assert.equal('fizz 22 23 fizz buzz ', fizzbuzz.createMessage(21, 25));
+			assert.equal('fizz 22 lucky fizz buzz ', fizzbuzz.createMessage(21, 25));
 		});
 	});
 })
