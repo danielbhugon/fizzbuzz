d05d88a32789262c8b7aecf7506427e8d2cbb4f1
Daniel <danielbhugon@gmail.com>
2016-01-15 20:56:52 +0000

    Translate function and unit test stub

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -1,4 +1,12 @@
 var assert = require('assert');
 var fizzbuzz = require('../lib/fizzbuzz.js');
 
-
+describe('fizzbuzz', function() {
+	
+	describe('#translate()', function () {
+		
+		it('should return "fizz" for the number 3', function () {
+			assert.equal(fizzbuzz.translate(3), 'fizz');
+		});
+	});
+})
