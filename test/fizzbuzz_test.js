5c987633bbebbab4cc7785064953c6fa24cb25f8
Daniel <danielbhugon@gmail.com>
2016-01-15 21:14:49 +0000

    Unit test cases built on first 20 examples (plus 0)

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -5,8 +5,14 @@ describe('fizzbuzz', function() {
 	
 	describe('#translate()', function () {
 		
-		it('should return "fizz" for the number 3', function () {
-			assert.equal(fizzbuzz.translate(3), 'fizz');
+		var testCases = ['fizzbuzz', '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz', '16'];
+		
+		testCases.forEach(function(expected, index){
+			it('should return ' + expected + ' for input ' + index, function () {
+				assert.equal(expected, fizzbuzz.translate(index));
+			});
 		});
+		
+		
 	});
 })
